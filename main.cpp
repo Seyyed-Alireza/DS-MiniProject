f7b6a1c546e4c73a95584c2ed6c91ee76305b27b
Arash Aghapour <arashaghapour13@gmail.com>
2025-11-27 13:12:44 +0330

    kids of the tokens generated

--- main.cpp
+++ main.cpp
@@ -2,14 +2,41 @@
 #include <regex>
 #include <string>
 #include <cmath>
-
+#include <utility>
 using namespace std;
 // sample input: (++2 + 3 / 22) ++-+- √3 * (+90 - 22 / 2) ++-+- 5 * √(3+2)
+
+int operation[59];
 
 struct Token {
     string kind;
     string str_value;
     float num_value;
+};
+
+pair<vector<Token>, vector<Token>> generate_kid (vector<Token> tokens) {
+    char min_op = ']';
+    int min_op_index = 0;
+    for (int i = 0; i < tokens.size(); i++) {
+        if (tokens[i].kind != "operation") {
+            continue;
+        }
+        if (operation[tokens[i].str_value[0] - 36] <= operation[min_op - 36]) {
+            min_op = tokens[i].str_value[0];
+            min_op_index = i;
+        }
+    }
+    return make_pair(vector<Token>(tokens.begin() + 0, tokens.begin() + min_op_index + 1), vector<Token>(tokens.begin() + min_op_index + 1, tokens.begin() + tokens.size()));
+}
+
+
+
+struct Node {
+    int id;
+    string kind;
+    string str_value;
+    Node* left_child;
+    Node* right_child;
 };
 
 string newNumberValue(int& index, string result) {
@@ -37,8 +64,19 @@ string newParenthesesValue(int& index, string result) {
     new_value.pop_back();
     return new_value;
 }
+void build_tree(vector<Token> tokens) {
+    generate_kid();
 
+}
 int main() {
+    operation['$' - 36] = 6;
+    operation['+' - 36] = 1;
+    operation['-' - 36] = 1;
+    operation['*' - 36] = 2;
+    operation['/' - 36] = 2;
+    operation['^' - 36] = 6;
+    operation['^' - 1] = 10;
+
     string input; getline(cin, input);
     regex sqrt_pattern("\xFB");
     input = regex_replace(input, sqrt_pattern, "$");
@@ -143,11 +181,28 @@ int main() {
 
         index++;
     }
-
-    cout << result << endl;
-    for (int i = 0; i < tokens.size(); i++) {
-        if (tokens[i].kind == "number") cout << tokens[i].num_value << ' ';
-        else cout << tokens[i].str_value << ' ';
-    }
+    // auto res = generate_kid(tokens);
+    // vector<Token> v1 = res.first;
+    // vector<Token> v2 = res.second;
+    // for (int i = 0; i < v1.size(); i++) {
+    //     if (v1[i].kind == "number") cout << v1[i].num_value << ' ';
+    //     else cout << v1[i].str_value << ' ';
+    // }
+    // cout << endl;
+    // for (int i = 0; i < v2.size(); i++) {
+    //     if (v2[i].kind == "number") cout << v2[i].num_value << ' ';
+    //     else cout << v2[i].str_value << ' ';
+    // }
+    // cout << endl;
+    // cout << result << endl;
+    // for (int i = 0; i < tokens.size(); i++) {
+    //     if (tokens[i].kind == "number") cout << tokens[i].num_value << ' ';
+    //     else cout << tokens[i].str_value << ' ';
+    // }
+    ofstream file("tree");
+    auto root = generate_kid(tokens);
+    vector<Token> left_ch = root.first;
+    vector<Token> right_ch = root.second;
+    
     return 0;
 }
